8b15e94e1b92e5ff81568bad8b9258be3dbda5cd
Avi Weinstock <aweinstock314@gmail.com>
2015-08-19 13:20:33 -0400

    Use the fact that clipboard_win::set_clipboard takes an AsRef<OsString> to shorten the invokation, as suggested in issue #13. Closes issue #13.

--- src/windows_clipboard.rs
+++ src/windows_clipboard.rs
@@ -1,6 +1,5 @@
 use clipboard_win::{get_clipboard_string, set_clipboard};
 
-use std::ffi::OsString;
 use std::error::Error;
 
 pub struct ClipboardContext;
@@ -13,6 +12,6 @@ impl ClipboardContext {
         Ok(try!(get_clipboard_string()))
     }
     pub fn set_contents(&self, data: String) -> Result<(), Box<Error>> {
-        Ok(try!(set_clipboard(&OsString::from(data))))
+        Ok(try!(set_clipboard(&data)))
     }
 }
