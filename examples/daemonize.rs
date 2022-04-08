7f248a7ed4bb38c9e0b0e43e7247f3febe6df6f7
SabrinaJewson <sejewson@gmail.com>
2022-04-08 16:06:32 +0100

    Allow daemonize to compile on all platforms

--- examples/daemonize.rs
+++ examples/daemonize.rs
@@ -1,25 +1,32 @@
 //! Example showcasing the use of `set_text_wait` and spawning a daemon to allow the clipboard's
 //! contents to live longer than the process on Linux.
 
-use arboard::{Clipboard, ClipboardExtLinux};
+use arboard::Clipboard;
+#[cfg(target_os = "linux")]
+use arboard::ClipboardExtLinux;
 use simple_logger::SimpleLogger;
 use std::{env, error::Error, process};
 
 fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+	#[cfg(target_os = "linux")]
 	if env::args().nth(1).as_deref() == Some("__internal_daemonize") {
-		Clipboard::new()?.set_text_wait("Hello, world!".into()).unwrap();
+		Clipboard::new()?.set_text_wait("Hello, world!".into())?;
 		return Ok(());
 	}
 
 	let _logger = SimpleLogger::new().init().unwrap();
 
-	process::Command::new(env::current_exe()?)
-		.arg("__internal_daemonize")
-		.stdin(process::Stdio::null())
-		.stdout(process::Stdio::null())
-		.stderr(process::Stdio::null())
-		.current_dir("/")
-		.spawn()?;
+	if cfg!(target_os = "linux") {
+		process::Command::new(env::current_exe()?)
+			.arg("__internal_daemonize")
+			.stdin(process::Stdio::null())
+			.stdout(process::Stdio::null())
+			.stderr(process::Stdio::null())
+			.current_dir("/")
+			.spawn()?;
+	} else {
+		Clipboard::new()?.set_text("Hello, world!".into())?;
+	}
 
 	Ok(())
 }
