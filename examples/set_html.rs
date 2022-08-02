0e38a4774b6b0bb9279b94a42b6cf7e0967bc489
Rodolphe Bréard <rodolphe@what.tf>
2022-08-02 10:54:58 +0200

    Add an example for set_html

---
+++ examples/set_html.rs
@@ -0,0 +1,15 @@
+use arboard::Clipboard;
+use std::{thread, time::Duration};
+
+fn main() {
+	let mut ctx = Clipboard::new().unwrap();
+
+	let text = "Hello, World!\n\
+        Lorem ipsum dolor sit amet,\n\
+        consectetur adipiscing elit.";
+	let html = "<h1>Hello, World!</h1>\
+        <b>Lorem ipsum</b> dolor sit amet,<br>\
+        <i>consectetur adipiscing elit</i>.";
+	ctx.set_html(html, Some(text)).unwrap();
+	thread::sleep(Duration::from_secs(5));
+}
