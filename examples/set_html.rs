10c13096a677a002275c66d576c3e518da364968
Rodolphe Bréard <rodolphe@what.tf>
2022-10-08 12:40:50 +0200

    Reformat strings in the set_html example

--- examples/set_html.rs
+++ examples/set_html.rs
@@ -4,15 +4,16 @@ use std::{thread, time::Duration};
 
 fn main() {
 	SimpleLogger::new().init().unwrap();
-
 	let mut ctx = Clipboard::new().unwrap();
 
-	let text = "Hello, World!\n\
-        Lorem ipsum dolor sit amet,\n\
-        consectetur adipiscing elit.";
-	let html = "<h1>Hello, World!</h1>\
-        <b>Lorem ipsum</b> dolor sit amet,<br>\
-        <i>consectetur adipiscing elit</i>.";
-	ctx.set_html(html, Some(text)).unwrap();
+	let html = r#"<h1>Hello, World!</h1>
+<b>Lorem ipsum</b> dolor sit amet,<br>
+<i>consectetur adipiscing elit</i>."#;
+
+	let alt_text = r#"Hello, World!
+Lorem ipsum dolor sit amet,
+consectetur adipiscing elit."#;
+
+	ctx.set_html(html, Some(alt_text)).unwrap();
 	thread::sleep(Duration::from_secs(5));
 }
